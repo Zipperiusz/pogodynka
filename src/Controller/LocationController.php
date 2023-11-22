13699e1035bafa7567597f80e1c4c7f69512524f
Zipperiusz <Zipperiusz0@gmail.com>
2023-11-22 18:01:09 +0100

    Revert "labe"
    This reverts commit 5ff2f597e21b9e99fb9c90024a5bb3c2eb3e2b5c.

--- src/Controller/LocationController.php
+++ src/Controller/LocationController.php
@@ -10,14 +10,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Attribute\IsGranted;
-
 
 #[Route('/location')]
 class LocationController extends AbstractController
 {
     #[Route('/', name: 'app_location_index', methods: ['GET'])]
-    #[IsGranted('ROLE_ADMIN')]
     public function index(LocationRepository $locationRepository): Response
     {
         return $this->render('location/index.html.twig', [
@@ -26,7 +23,6 @@ class LocationController extends AbstractController
     }
 
     #[Route('/new', name: 'app_location_new', methods: ['GET', 'POST'])]
-    #[IsGranted('ROLE_LOCATION_NEW')]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $location = new Location();
@@ -49,7 +45,6 @@ class LocationController extends AbstractController
     }
 
     #[Route('/{id}', name: 'app_location_show', methods: ['GET'])]
-    #[IsGranted('ROLE_LOCATION_SHOW')]
     public function show(Location $location): Response
     {
         return $this->render('location/show.html.twig', [
@@ -58,7 +53,6 @@ class LocationController extends AbstractController
     }
 
     #[Route('/{id}/edit', name: 'app_location_edit', methods: ['GET', 'POST'])]
-    #[IsGranted('ROLE_LOCATION_EDIT')]
     public function edit(Request $request, Location $location, EntityManagerInterface $entityManager): Response
     {
          $form = $this->createForm(LocationType::class, $location, [
@@ -79,7 +73,6 @@ class LocationController extends AbstractController
     }
 
     #[Route('/{id}', name: 'app_location_delete', methods: ['POST'])]
-    #[IsGranted('ROLE_LOCATION_DELETE')]
     public function delete(Request $request, Location $location, EntityManagerInterface $entityManager): Response
     {
         if ($this->isCsrfTokenValid('delete'.$location->getId(), $request->request->get('_token'))) {
--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -11,14 +11,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Attribute\IsGranted;
-
 
 #[Route('/weather')]
 class WeatherController extends AbstractController
 {
     #[Route('/', name: 'app_weather_index', methods: ['GET'])]
-    #[IsGranted('ROLE_WEATHER_INDEX')]
     public function index(WeatherRepository $weatherRepository, LocationRepository $locationRepository): Response
     {
         return $this->render('weather/index.html.twig', [
@@ -28,7 +25,6 @@ class WeatherController extends AbstractController
     }    
 
     #[Route('/new', name: 'app_weather_new', methods: ['GET', 'POST'])]
-    #[IsGranted('ROLE_WEATHER_NEW')]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $weather = new Weather();
@@ -51,7 +47,6 @@ class WeatherController extends AbstractController
     }
 
     #[Route('/{id}', name: 'app_weather_show', methods: ['GET'])]
-    #[IsGranted('ROLE_WEATHER_SHOW')]
     public function show(Weather $weather): Response
     {
         return $this->render('weather/show.html.twig', [
@@ -60,7 +55,6 @@ class WeatherController extends AbstractController
     }
 
     #[Route('/{id}/edit', name: 'app_weather_edit', methods: ['GET', 'POST'])]
-    #[IsGranted('ROLE_WEATHER_EDIT')]
     public function edit(Request $request, Weather $weather, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(WeatherType::class, $weather,  [
@@ -81,7 +75,6 @@ class WeatherController extends AbstractController
     }
 
     #[Route('/{id}', name: 'app_weather_delete', methods: ['POST'])]
-    #[IsGranted('ROLE_WEATHER_DELETE')]
     public function delete(Request $request, Weather $weather, EntityManagerInterface $entityManager): Response
     {
         if ($this->isCsrfTokenValid('delete'.$weather->getId(), $request->request->get('_token'))) {
